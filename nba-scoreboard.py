3c7d46d202e0fec2eac9d03e1c8d81fa5b01862d
anoopravella <34986736+anoopravella@users.noreply.github.com>
2018-01-03 16:28:19 -0500

    created clean_data helper function

--- nba-scoreboard.py
+++ nba-scoreboard.py
@@ -2,9 +2,20 @@ from lxml import html
 import requests
 import sys
 
+# function to remove 
+def clean_data (raw_list, destination_list) :
+	for element in raw_list :
+		element = element.replace('\n', '')
+		element = element.replace('\x95', '')
+		element = element.strip()
+		destination_list.append(element)
+
+	return destination_list
+
 
 # main program
 
+# processing arguments, error checking and handling
 if len(sys.argv) > 2 :
 	print ('Error')
 	print ('USAGE: nba-scoreboard.py [date in YYYY-MM-DD form]')
@@ -30,7 +41,7 @@ gameStatus = []
 awayScores = []
 homeScores = []
 
-
+# scraping city, team name, and team record data
 awayTeamsCityRaw = tree.xpath('//div[@class="teams"]/div[1]/div[2]/div[1]/a/text()')
 awayTeamsName = tree.xpath('//div[@class="teams"]/div[1]/div[2]/a[2]/span/text()')
 awayTeamsRecordRaw = tree.xpath('//div[@class="teams"]/div[1]/div[2]/span/text()')
@@ -39,94 +50,52 @@ homeTeamsCityRaw = tree.xpath('//div[@class="teams"]/div[3]/div[2]/div[1]/a/text
 homeTeamsName = tree.xpath('//div[@class="teams"]/div[3]/div[2]/a[2]/span/text()')
 homeTeamsRecordRaw = tree.xpath('//div[@class="teams"]/div[3]/div[2]/span/text()')
 
-for city in awayTeamsCityRaw :
-	city = city.replace('\n', '')
-	city = city.strip()
-	awayTeamsCityProcessed.append(city)
-
-for city in homeTeamsCityRaw :
-	city = city.replace('\n', '')
-	city = city.strip()
-	homeTeamsCityProcessed.append(city)
-
-for record in awayTeamsRecordRaw :
-	record = record.replace('\n', '')
-	record = record.strip()
-	awayTeamsRecordProcessed.append(record)
-
-for record in homeTeamsRecordRaw :
-	record = record.replace('\n', '')
-	record = record.strip()
-	homeTeamsRecordProcessed.append(record)
+# cleaning up city and record data
+awayTeamsCityProcessed = clean_data(awayTeamsCityRaw, awayTeamsCityProcessed)
+homeTeamsCityProcessed = clean_data(homeTeamsCityRaw, homeTeamsCityProcessed)
+awayTeamsRecordProcessed = clean_data(awayTeamsRecordRaw, awayTeamsRecordProcessed)
+homeTeamsRecordProcessed = clean_data(homeTeamsRecordRaw, homeTeamsRecordProcessed)
 
 
 # games in progress
 if tree.xpath('//div[@class="status-active uppercase"]/text()') :
+	
 	clocks = tree.xpath('//span[@class="status-active uppercase"]/text()')
-
-	for clock in clocks :
-		clock = clock.replace('\n', '')
-		clock = clock.replace('\x95', '')
-		clock = clock.strip()
-		gameStatus.append(clock)
-
+	gameStatus = clean_data(clocks, gameStatus)
 
 	awayScoresRaw = tree.xpath("//div[@class='status-active uppercase']/../../../div[2]/div/div[1]/div/div[1]/div[3]/div/text()")
-
-	for score in awayScoresRaw :
-		score = score.replace('\n', '')
-		score = score.strip()
-		awayScores.append(score)
+	awayScores = clean_data(awayScoresRaw, awayScores)
 
 	homeScoresRaw = tree.xpath("//div[@class='status-active uppercase']/../../../div[2]/div/div[1]/div/div[3]/div[3]/div/text()")
-
-	for score in homeScoresRaw :
-		score = score.replace('\n', '')
-		score = score.strip()
-		homeScores.append(score)
+	homeScores = clean_data(homeScoresRaw, homeScores)
 
 # games not yet started
 if tree.xpath('//div[@class="float-left status-container"]/strong/text()') :
-	times = tree.xpath('//div[@class="float-left status-container"]/strong/text()')
 	
-	for time in times :
-		time = time.replace('\n', '')
-		time = time.strip()
-		gameStatus.append(time)
+	times = tree.xpath('//div[@class="float-left status-container"]/strong/text()')
+	gameStatus = clean_data(times, gameStatus)
 
+	for time in times :
 		awayScores.append('-')
 		homeScores.append('-')
+	
 
 # games ended
 if tree.xpath('//div[@class="status-final"]/text()') :
+
 	finals = tree.xpath('//div[@class="status-final"]/text()')
-
-	for final in finals :
-		final = final.replace('\n', '')
-		final = final.strip()
-		gameStatus.append(final)
-
+	gameStatus = clean_data(finals, gameStatus)
 
 	awayScoresRaw = tree.xpath('//div[@class="status-final"]/../../../div[2]/div/div[1]/div/div[1]/div[3]/div/text()')
-
-	for score in awayScoresRaw :
-		score = score.replace('\n', '')
-		score = score.strip()
-		awayScores.append(score)
+	awayScores = clean_data(awayScoresRaw, awayScores)
 
 	homeScoresRaw = tree.xpath('//div[@class="status-final"]/../../../div[2]/div/div[1]/div/div[3]/div[3]/div/text()')
-
-	for score in homeScoresRaw :
-		score = score.replace('\n', '')
-		score = score.strip()
-		homeScores.append(score)
-
-
+	homeScores = clean_data(homeScoresRaw, homeScores)
 
 
 gameStatus = list(filter(None, gameStatus))		# remove empty strings from list
 
-
+# prints the scoreboard
 for awayCity, homeCity, awayTeam, homeTeam, awayRecord, homeRecord, status, awayScore, homeScore in zip(awayTeamsCityProcessed, homeTeamsCityProcessed, awayTeamsName, homeTeamsName, awayTeamsRecordProcessed, homeTeamsRecordProcessed, gameStatus, awayScores, homeScores) :
 	print ('{0:50} {1:10}'.format(awayCity + ' ' + awayTeam + ' ' + awayRecord, awayScore))
 	print ('{0:50} {1:10}'.format(homeCity + ' ' + homeTeam + ' ' + homeRecord, homeScore))
